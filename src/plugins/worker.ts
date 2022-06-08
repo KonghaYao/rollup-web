0445b50443c77ef444b951bde28efc268633226c
konghayao <3446798488@qq.com>
2022-06-08 18:38:22 +0800

    build: 构建 3.7.6

--- src/plugins/worker.ts
+++ src/plugins/worker.ts
@@ -64,7 +64,7 @@ const WorkerWrapperCode = function (options?: WorkerOptions) {
 };
 
 // 使用了线上版本的 worker 辅助
-const isOnline = false;
+const isOnline = true;
 const moduleWorkerURL = await createLocalModule(
     isOnline
         ? Setting.NPM(
