75a0cf7e43c942145a656fbaae4013c6450bbade
konghayao <3446798488@qq.com>
2022-06-09 09:54:17 +0800

    feat: 测试通过

--- public/assembly/index.js
+++ public/assembly/index.js
@@ -1,2 +1,11 @@
-import * as bundle from "./index.ts?assemblyscript";
-console.log(bundle);
+import * as exports from "./index.ts?assemblyscript";
+console.log(exports);
+const log = (...args) => console.log(args);
+
+// Randomize the array in WebAssembly and log it again
+const result = exports.randomizeArray(new Uint32Array([1, 2, 3, 4]));
+log("Randomized values: ", result);
+
+// Compute the array values' sum and log it. This will overflow i32 range.
+let total = exports.sumArray(new Uint32Array([1, 2, 3, 4]));
+log(`Sum (likely overflown): `, total);
--- public/assembly/index.ts
+++ public/assembly/index.ts
@@ -1,51 +1,17 @@
-//https://github.com/AssemblyScript/examples/blob/main/interference/assembly/index.ts
-var width = 320;
-var height = 200;
-
-export const offset = __heap_base;
-
-function set(x: i32, y: i32, v: f32): void {
-    var vi = <i32>v;
-    store<i32>(offset + ((width * y + x) << 2), (~vi << 24) | (vi << 8));
+/** Randomizes the specified array's values. */
+export function randomizeArray(arr: Int32Array): Int32Array {
+    for (let i = 0, k = arr.length; i < k; ++i) {
+        let value = i32((Math.random() * 2.0 - 1.0) * i32.MAX_VALUE);
+        unchecked((arr[i] = value));
+    }
+    return arr;
 }
 
-function distance(x1: i32, y1: i32, x2: f32, y2: f32): f32 {
-    var dx = <f32>x1 - x2;
-    var dy = <f32>y1 - y2;
-    return Mathf.sqrt(dx * dx + dy * dy);
-}
-
-export function update(tick: f32): void {
-    var w = <f32>width;
-    var h = <f32>height;
-    var hw = w * 0.5,
-        hh = h * 0.5;
-    var cx1 = (Mathf.sin(tick * 2) + Mathf.sin(tick)) * hw * 0.3 + hw,
-        cy1 = Mathf.cos(tick) * hh * 0.3 + hh,
-        cx2 = (Mathf.sin(tick * 4) + Mathf.sin(tick + 1.2)) * hw * 0.3 + hw,
-        cy2 = (Mathf.sin(tick * 3) + Mathf.cos(tick + 0.1)) * hh * 0.3 + hh;
-    var res = <f32>48 / Mathf.max(w, h);
-    var y = 0;
-    do {
-        let x = 0;
-        do {
-            set(
-                x,
-                y,
-                Mathf.abs(
-                    Mathf.sin(distance(x, y, cx1, cy1) * res) +
-                        Mathf.sin(distance(x, y, cx2, cy2) * res)
-                ) * 120
-            );
-        } while (++x != width);
-    } while (++y != height);
-}
-
-export function resize(w: i32, h: i32): void {
-    width = w;
-    height = h;
-    var needed =
-        (<i32>((offset + (w * h * sizeof<i32>() + 0xffff)) & ~0xffff)) >>> 16;
-    var actual = memory.size();
-    if (needed > actual) memory.grow(needed - actual);
+/** Computes the sum of an array's values and returns the sum to JavaScript. */
+export function sumArray(arr: Int32Array): i32 {
+    let total = 0;
+    for (let i = 0, k = arr.length; i < k; ++i) {
+        total += unchecked(arr[i]);
+    }
+    return total;
 }
