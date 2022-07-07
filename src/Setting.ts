b71bf7dffbe5d05009d3dc7ae544d21a9b594c77
konghayao <3446798488@qq.com>
2022-07-07 10:55:26 +0800

    build:4.3.3

--- src/Setting.ts
+++ src/Setting.ts
@@ -3,7 +3,7 @@ import { useGlobal } from "./utils/useGlobal";
 export const Setting = {
     NPM: (path: string) => "https://fastly.jsdelivr.net/npm/" + path,
     // worker 中有使用了 cdn 中的代码进行操作，故而需要 rollup-web 版本支持
-    workerVersion: "4.3.2",
+    workerVersion: "4.3.3",
     async loadSystemJS() {
         const systemURL = this.NPM("systemjs@6.12.1/dist/system.min.js");
 
