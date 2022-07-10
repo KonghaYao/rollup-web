8db5dcf9aadb12d51a769e3a280629e0f92c9f80
konghayao <3446798488@qq.com>
2022-07-10 12:20:16 +0800

    fixed: 修复 mdx 问题

--- src/plugins/mdx.ts
+++ src/plugins/mdx.ts
@@ -1,7 +1,44 @@
 import { Plugin } from "rollup-web";
 import { wrapPlugin } from "../utils/wrapPlugin";
-import { compile, CompileOptions } from "@mdx-js/mdx";
+// TODO 在 worker 中会因为没有 dom 而报错，可能是 cdn 使用 browser 问题
+import type { CompileOptions } from "@mdx-js/mdx/lib/compile";
+
 import merge from "lodash-es/merge";
+let compile: typeof import("@mdx-js/mdx/lib/compile")["compile"];
+
+const pluginFixed = async () => {
+    if (!globalThis.document) {
+        // mdx 在 worker 中构造失败，所以使用这种方式进行一个保全
+        /* @ts-ignore */
+        const { decodeNamedCharacterReference } = await import(
+            "https://fastly.jsdelivr.net/npm/decode-named-character-reference@1.0.2/index.js/+esm"
+        );
+        let info = "";
+        const mdxFakeElement = new Proxy(
+            {},
+            {
+                get() {
+                    return info;
+                },
+                set(_, __, data) {
+                    info = decodeNamedCharacterReference(data);
+                    return true;
+                },
+            }
+        );
+        // 在这里让后面的代码可以见到 document，然后立马删除 document 避免影响
+        globalThis.document = {
+            /* @ts-ignore */
+            createElement() {
+                return mdxFakeElement;
+            },
+        };
+        return () => {
+            /* @ts-ignore */
+            globalThis.document = undefined;
+        };
+    }
+};
 
 export const _mdx = ({
     options,
@@ -13,6 +50,14 @@ export const _mdx = ({
 } = {}) => {
     return {
         name: "mdx",
+        async buildStart() {
+            if (!compile) {
+                const func = await pluginFixed();
+                const module = await import("@mdx-js/mdx/lib/compile.js");
+                func && func();
+                compile = module.compile;
+            }
+        },
         /** wrapPlugin 提供了 extensions 守护，id 必然是符合的 */
         async transform(code: string, id: string) {
             const file = await compile(
