c98e4c4871af35331954783e0697acc885a27f91
konghayao <3446798488@qq.com>
2022-07-12 09:28:29 +0800

    build: 4.4.0

--- src/Setting.ts
+++ src/Setting.ts
@@ -3,7 +3,8 @@ import { useGlobal } from "./utils/useGlobal";
 export const Setting = {
     NPM: (path: string) => "https://fastly.jsdelivr.net/npm/" + path,
     // worker 中有使用了 cdn 中的代码进行操作，故而需要 rollup-web 版本支持
-    version: "4.3.3",
+    // iframe 中也需要
+    version: "4.4.0",
     async loadSystemJS() {
         const systemURL = this.NPM("systemjs@6.12.1/dist/system.min.js");
 
